467e6504580a3b3089e78d89a815b2c0fffef72c
Allan <mail@allanreid.co>
2022-01-03 13:22:29 -0800

    search css done

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tootle</title>
-
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
@@ -14,21 +14,19 @@
 
         <div class="mainSection">
 
-            <div class="logoContainer"></div>
+            <div class="logoContainer">
+                <img src="assets/images/noodleLogo.png">
+            </div>
 
-            <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png">
+            <div class="searchContainer">
+
+                <form action="search.php" method="GET">
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="Search">
+                </form>
+            </div>
         </div>
-
-        <div class="searchContainer">
-
-            <form action="search.php" method="GET">
-                <input class="searchBox" type="text" name="term">
-                <input class="searchButton" type="submit" value="Search">
-
-            </form>
-        </div>
-
-        </div>
+    </div>
 
 </body>
 </html>
--- search.php
+++ search.php
@@ -1,3 +1,49 @@
-<?php 
-echo $_GET["term"];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tootle</title>
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="wrapper ">
+
+        <div class="header">
+
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/images/noodleLogo.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+
+                    <form action="search.php" method="GET">
+
+                        <div class="searchBarContainer">
+
+                            <input class="searchBox" type="text" name="term">
+                            <button class="searchButton">
+                                <img src="assets/images/icons/search.png" alt="">
+                            </button>
+
+                        </div>
+
+                    </form>
+
+                </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+
+</body>
+</html>
