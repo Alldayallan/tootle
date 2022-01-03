e18f913679d3f0655b52f24f92302ee27490d500
Allan <mail@allanreid.co>
2022-01-03 08:39:49 -0800

    back at it

--- index.php
+++ index.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tootle</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-    
+
+    <div class="wrapper indexPage">
+
+        <div class="mainSection">
+
+            <div class="logoContainer"></div>
+
+            <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png">
+        </div>
+
+        <div class="searchContainer">
+
+            <form action="search.php" method="GET">
+                <input class="searchBox" type="text" name="term">
+                <input class="searchButton" type="submit" value="Search">
+
+            </form>
+        </div>
+
+        </div>
+
 </body>
 </html>
